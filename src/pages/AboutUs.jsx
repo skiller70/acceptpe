b4be85fab30381ce21c46e384f08ebb6b3914456
lucky <singhlucky007007@gmail.com>
2023-06-28 18:21:13 +0530

    added icon in about page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect } from 'react'
 import aboutus from "/aboutus.png"
 import Footer from "../components/Footer"
+import CustomerEx from "/CustomerEx.png"
+import InsightDriven from "/InsightDriven.png"
+import Customer from "/Customer.png"
+import AdvancedA from "/AdvancedA.png"
+import PaymentT  from "/PaymentT.png"
+import SecureP from "/SecureP.png"
 
 function AboutUs() {
 
@@ -10,7 +16,7 @@ function AboutUs() {
   return (
     <div>
       <div className=' '>
-        <img src={aboutus}   alt="" />
+        <img src={aboutus} alt="" />
       </div>
 
 
@@ -19,17 +25,21 @@ function AboutUs() {
         <br />
         <div className=' grid grid-cols-1 md:grid-cols-3 gap-10'>
 
-        
-          <div className=' shadow-md py-20 px-10 border-2 border-[#ECBD00] rounded-lg'>
+
+          <div className=' shadow-md py-10 px-10 border-2 border-[#ECBD00] rounded-lg'>
+            <img src={InsightDriven} alt="" />
+            <br />
             <h1 className=' text-xl'> Insight-driven Tools and Services</h1>
             <br />
             <p>Because ultimately, it is the insights that drive value, letting you have an advantage over your competitors.</p>
           </div>
 
 
-       
 
-          <div className=' shadow-md py-20 px-10 border-2 border-[#ECBD00] rounded-lg'>
+
+          <div className=' shadow-md py-10 px-10 border-2 border-[#ECBD00] rounded-lg'>
+            <img src={Customer} alt="" />
+            <br />
             <h1 className=' text-xl'> Customer-oriented Service</h1>
             <br />
             <p>At Easebuzz, its all about focusing on helping customers meet their goals.</p>
@@ -37,13 +47,17 @@ function AboutUs() {
 
 
 
-          <div className=' shadow-md py-20 px-10 border-2 border-[#ECBD00] rounded-lg'>
+          <div className=' shadow-md py-10 px-10 border-2 border-[#ECBD00] rounded-lg'>
+              <img src={CustomerEx} alt="" />
+              <br />
             <h1 className=' text-xl'> Better Customer Experience & Interaction</h1>
             <br />
             <p>We believe in keeping our customers happy through Simpler and Convenient Payment Processes.</p>
           </div>
 
-          <div className=' shadow-md py-20 px-10 border-2 border-[#ECBD00] rounded-lg'>
+          <div className=' shadow-md py-10 px-10 border-2 border-[#ECBD00] rounded-lg'>
+            <img src={AdvancedA} alt="" />
+            <br />
             <h1 className=' text-xl'> Advanced Analytics</h1>
             <br />
             <p>Our digital solutions enable SMEs to streamline and offer customers the convenience of self-service digital experiences.</p>
@@ -51,13 +65,17 @@ function AboutUs() {
 
 
 
-          <div className=' shadow-md py-20 px-10 border-2 border-[#ECBD00] rounded-lg'>
+          <div className=' shadow-md py-10 px-10 border-2 border-[#ECBD00] rounded-lg'>
+            <img src={SecureP} alt="" />
+            <br />
             <h1 className=' text-xl'>Secure in Every Step</h1>
             <br />
             <p>Dedicated to providing the highest level of secure digital payment solutions.</p>
           </div>
 
-          <div className=' shadow-md py-20 px-10 border-2 border-[#ECBD00] rounded-lg'>
+          <div className=' shadow-md py-10 px-10 border-2 border-[#ECBD00] rounded-lg'>
+            <img src={PaymentT} alt="" />
+            <br />
             <h1 className=' text-xl'>Real time Payment Tracking & Analytics</h1>
             <br />
             <p>We provide Real-Time Payment Analytics that helps drive more business and minimize customer dropouts.</p>
@@ -75,9 +93,9 @@ function AboutUs() {
 
       <div className='px-8 md:px-32 text-xl   leading-8 font-normal  '>
         AcceptPe is a payment gateway that enables businesses to accept online payments securely and easily. With a focus on user experience, AcceptPe offers a simple and intuitive interface that allows merchants to easily manage transactions, view reports, and access customer support. Our payment platform uses advanced security protocols to ensure the safety of your customers' data, and our competitive pricing structure makes it an affordable solution for businesses of all sizes. At AcceptPe, we're committed to helping businesses succeed by providing a reliable and efficient payment processing service.
-        </div>
+      </div>
 
-      <Footer/>
+      <Footer />
     </div>
   )
 }
