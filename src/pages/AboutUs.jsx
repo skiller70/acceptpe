2afef0c4f36c5f8bfd480f7a6bc10110bb1911bf
lucky <singhlucky007007@gmail.com>
2023-06-28 17:56:23 +0530

    added about us contetnt

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,22 +1,83 @@
 import React, { useEffect } from 'react'
 import aboutus from "/aboutus.png"
+import Footer from "../components/Footer"
 
 function AboutUs() {
 
-    useEffect(() => {
-        window.scrollTo(0, 0)
-      }, [])
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
   return (
     <div>
-        <div>
-            <img src={aboutus} alt="" />
+      <div className=' '>
+        <img src={aboutus}   alt="" />
+      </div>
+
+
+      <div className='  my-16 mx-8 md:mx-32'>
+        <h1 className=' text-center font-semibold text-2xl my-8'>Why Choose US</h1>
+        <br />
+        <div className=' grid grid-cols-1 md:grid-cols-3 gap-10'>
+
+        
+          <div className=' shadow-md py-20 px-10 border-2 border-[#ECBD00] rounded-lg'>
+            <h1 className=' text-xl'> Insight-driven Tools and Services</h1>
+            <br />
+            <p>Because ultimately, it is the insights that drive value, letting you have an advantage over your competitors.</p>
+          </div>
+
+
+       
+
+          <div className=' shadow-md py-20 px-10 border-2 border-[#ECBD00] rounded-lg'>
+            <h1 className=' text-xl'> Customer-oriented Service</h1>
+            <br />
+            <p>At Easebuzz, its all about focusing on helping customers meet their goals.</p>
+          </div>
+
+
+
+          <div className=' shadow-md py-20 px-10 border-2 border-[#ECBD00] rounded-lg'>
+            <h1 className=' text-xl'> Better Customer Experience & Interaction</h1>
+            <br />
+            <p>We believe in keeping our customers happy through Simpler and Convenient Payment Processes.</p>
+          </div>
+
+          <div className=' shadow-md py-20 px-10 border-2 border-[#ECBD00] rounded-lg'>
+            <h1 className=' text-xl'> Advanced Analytics</h1>
+            <br />
+            <p>Our digital solutions enable SMEs to streamline and offer customers the convenience of self-service digital experiences.</p>
+          </div>
+
+
+
+          <div className=' shadow-md py-20 px-10 border-2 border-[#ECBD00] rounded-lg'>
+            <h1 className=' text-xl'>Secure in Every Step</h1>
+            <br />
+            <p>Dedicated to providing the highest level of secure digital payment solutions.</p>
+          </div>
+
+          <div className=' shadow-md py-20 px-10 border-2 border-[#ECBD00] rounded-lg'>
+            <h1 className=' text-xl'>Real time Payment Tracking & Analytics</h1>
+            <br />
+            <p>We provide Real-Time Payment Analytics that helps drive more business and minimize customer dropouts.</p>
+          </div>
+
+
+
+
         </div>
 
-        <div className='px-8 md:px-40 text-2xl py-16  font-normal  text-justify'>
+      </div>
+
+
+
+
+      <div className='px-8 md:px-32 text-xl   leading-8 font-normal  '>
         AcceptPe is a payment gateway that enables businesses to accept online payments securely and easily. With a focus on user experience, AcceptPe offers a simple and intuitive interface that allows merchants to easily manage transactions, view reports, and access customer support. Our payment platform uses advanced security protocols to ensure the safety of your customers' data, and our competitive pricing structure makes it an affordable solution for businesses of all sizes. At AcceptPe, we're committed to helping businesses succeed by providing a reliable and efficient payment processing service.
         </div>
 
-
+      <Footer/>
     </div>
   )
 }
